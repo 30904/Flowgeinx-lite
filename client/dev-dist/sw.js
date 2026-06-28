05af9687834afa23a4de41db23a26dbec9751126
unknown <arnavchikhale30@gmail.com>
2026-06-28 11:01:46 +0530

    chore: update PWA service worker build artifact

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-cc1d1889'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.ni79n31hkd"
+    "revision": "0.54lk0hmh7rk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
